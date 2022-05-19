1907324337218cda79c81d40b20414af1a40a45b
vishnu <105475692+vishnu-Ivar@users.noreply.github.com>
2022-05-19 11:30:14 +0530

    Add files via upload

---
+++ print sequence simple way.py
@@ -0,0 +1,9 @@
+# code to count the numbers
+""" we can use to print the even/odd or whatever we want """
+value = int(input("enter the target:"))
+Sum = 1
+while Sum<=value:
+    if Sum%2!=0:
+        print(Sum)
+    Sum+=1
+    continue
